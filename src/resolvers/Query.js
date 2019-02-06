2f7db2de330d56d613fa9acc1da6f7afaea9fdbc
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-05 22:40:57 -0800

    add items

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const {forwardTo} = require('prisma-binding');
+
+const Query = {
+    // If there is no need for authentication, filtering, custom logic, and the query is exactly the same between Yoga and Prisma.
+    items: forwardTo('db'),
+
+    // Alternate version
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
+};
 
 module.exports = Query;
