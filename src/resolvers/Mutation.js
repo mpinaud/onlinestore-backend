c218e7c4872cbee319e62ac4f31eeef2e3e332c4
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-05 22:41:40 -0800

    add item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutation = {};
+const Mutation = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
+    }
+};
 
 module.exports = Mutation;
