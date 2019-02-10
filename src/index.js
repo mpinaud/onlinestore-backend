7dc4e5eb1b6a43b34e05c315edfe7dd1b92f9a42
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-09 17:51:32 -0800

    create middleware to get userId using the JWT on each request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const cookieParser = require('cookie-parser');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config({path: 'variables.env'});
 const createServer = require('./createServer');
 const db = require('./db');
@@ -9,7 +9,16 @@ const server = createServer();
 // Use express middlware to handle cookies (JWT)
 server.express.use(cookieParser());
 
-// TODO Use express middlware to populate current user
+// Middleware to decode the JWT so that we can get the user ID on each request
+server.express.use((req, res, next) => {
+    const {token} = req.cookies;
+    if (token) {
+        const {userId} = jwt.verify(token, process.env.APP_SECRET);
+        // put the userId onto the req for the future requests to access
+        req.userId = userId
+    }
+    next();
+});
 
 server.start(
     {
