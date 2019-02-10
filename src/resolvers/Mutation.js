98b96ca657bcf06aee071c99d36e64daaaebe7b8
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-10 11:53:05 -0800

    create relationship between item and user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -7,9 +7,18 @@ const {transport, makeANiceEmail} = require('../mail');
 
 const Mutation = {
     async createItem(parent, args, ctx, info) {
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in to do that!');
+        }
         const item = await ctx.db.mutation.createItem(
             {
                 data: {
+                    // This is how to create a relationship between the Item and the User
+                    user: {
+                        connect: {
+                            id: ctx.request.userId
+                        }
+                    },
                     ...args,
                 },
             },
