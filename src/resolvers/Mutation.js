b83bc9a3f3f520043f229bb1ad72780b70b6259b
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-09 16:46:31 -0800

    create signup resolver mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem(
@@ -18,6 +21,33 @@ const Mutation = {
         // TODO
         // Delete it
         return ctx.db.mutation.deleteItem({where}, info);
+    },
+    async signup(parent, args, ctx, info) {
+        // lowercase the email
+        args.email = args.email.toLowerCase();
+        // hash their password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the database
+        const user = await ctx.db.mutation.createUser(
+            {
+                data: {
+                    ...args,
+                    password,
+                    // how to use an ENUM
+                    permissions: {set: ['USER']},
+                },
+            },
+            info
+        );
+        // create the JWT token for them
+        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+        // We set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+        });
+        // Finally return the user to the browser
+        return user;
     },
     updateItem(parent, args, ctx, info) {
         // First take a copy of the updates
