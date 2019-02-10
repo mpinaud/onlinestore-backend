a3a0dcfdab094fcf0f67b4e0812f07ca4010f72d
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-09 17:52:18 -0800

    create me to return a User and create query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,15 +2,26 @@ const {forwardTo} = require('prisma-binding');
 
 const Query = {
     // If there is no need for authentication, filtering, custom logic, and the query is exactly the same between Yoga and Prisma.
-    items: forwardTo('db'),
     item: forwardTo('db'),
     itemsConnection: forwardTo('db'),
-
+    items: forwardTo('db'),
     // Alternate version
     // async items(parent, args, ctx, info) {
     //     const items = await ctx.db.query.items();
     //     return items;
     // }
+    me(parent, args, ctx, info) {
+        // check if there is a current user ID
+        if (!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user(
+            {
+                where: { id: ctx.request.userId },
+            },
+            info
+        );
+    },
 };
 
 module.exports = Query;
